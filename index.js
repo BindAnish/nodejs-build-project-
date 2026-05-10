7c8347bc43a4a26223ef4dc75cc53267de1e8d48
Ubuntu <ubuntu@ip-172-31-9-156.us-east-2.compute.internal>
2026-05-10 16:28:53 +0000

    add deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello DevOps World!');
+    res.end('Hello DevOps World deploy!');
 });
 
 const PORT = 3000;
